a9a26b2f90c6cb73218505ed2662682bafe10fa5
Jorge Cabrera <alu0101351773@ull.edu.es>
2021-11-20 12:29:13 +0000

    Funcionamiento básico implementado

--- userProc.sh
+++ userProc.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-ps --no-header -u $USER -eo cputimes | awk '$1 > 4 {print $0;}'
+TEXT_BOLD=$(tput bold)
+TEXT_ULINE=$(tput sgr 0 1)
+TEXT_GREEN=$(tput setaf 2)
+TEXT_RESET=$(tput sgr0)
+
+
+function basic_function() {
+
+	user_list=$( ps --no-header -eo time,user | \
+				tr ':' ' ' | \
+				awk '{print $4, $1 * 60 * 60 + $2 * 60 + $3}' | \
+				awk '$2 > 1 {print $1;}' | \
+				sort -u | \
+				tr '\n' ' ' )
+
+	for user in $user_list; do
+		user_gid=$( id -g $user)
+		user_uid=$( id -u $user)
+		user_proc_num=$( ps --no-header -u $user | wc -l)
+		user_cpu=$(ps --no-header -u $user -eo pid,time | sort -k 2 -r | head -n 1 | tr -s ' ')
+		echo "$user $user_uid $user_proc_num $user_cpu"
+	done
+}
+
+basic_function 
+
+while []; do
+
+done
